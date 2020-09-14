1cdf0d8c20b7c53c3947231fd3423d7423865934
eunjin0212 <eunjin9639@gmail.com>
2020-09-14 18:45:20 +0900

    js

--- game.js
+++ game.js
@@ -1,7 +1,18 @@
-var word = "제로초";
-while (true) {
+// var word = "제로초";
+// while (true) {
+//   var answer = prompt(word);
+//   if (word[word.length - 1] === answer[0]) {
+//     alert("딩동댕");
+//     word = answer;
+//   } else {
+//     alert("끝말잇기입니다.");
+//   }
+// }
+
+for (var word = "제로초"; true; ) {
   var answer = prompt(word);
   if (word[word.length - 1] === answer[0]) {
+    alert("딩동댕");
     word = answer;
   } else {
     alert("끝말잇기입니다.");
--- test.js
+++ test.js
@@ -1,27 +1,58 @@
-var result = 3 * 5;
-console.log(result);
+// var result = 3 * 5;
+// console.log(result);
 
-var 빈값 = "";
-console.log(빈값);
+// var 빈값 = "";
+// console.log(빈값);
 
-빈값 = null;
+// 빈값 = null;
 
-console.log(빈값);
+// console.log(빈값);
 
-console.log(null == undefined);
-console.log(null === undefined);
-var 조건 = 5 * 5 === 25;
+// console.log(null == undefined);
+// console.log(null === undefined);
+// var 조건 = 5 * 5 === 25;
 
-if (조건) {
-  return "딩동댕";
-} else {
-  return "땡";
-}
-console.log(조건);
+// if (조건) {
+//   return "딩동댕";
+// } else {
+//   return "땡";
+// }
+// console.log(조건);
 
-if (false) {
-  ("딩동");
-} else {
-  ("땡");
-}
-console.log(false);
+// if (false) {
+//   ("딩동");
+// } else {
+//   ("땡");
+// }
+// console.log(false);
+
+// for (let star = 1; star <= 5; star = star += 1) {
+//   console.log("*".repeat(star));
+// }
+// for (let star = 5; star >= 1; star = star -= 1) {
+//   console.log("*".repeat(star));
+// }
+// for (let star = 2; star <= 10; star = star += 2) {
+//   console.log("*".repeat(star));
+// }
+// for (let star = 10; star >= 2; star = star -= 2) {
+//   console.log("*".repeat(star));
+// }
+// for (let star = 1; star <= 16; star = star *= 2) {
+//   console.log("*".repeat(star));
+// }
+// for (let star = 5; star >= 1; star -= 1) {
+//   console.log(" ".repeat(5 - star) + "*".repeat(star));
+// }
+// for (let star = 1; star <= 5; star += 1) {
+//   console.log(" ".repeat(5 - star) + "*".repeat(star));
+// }
+// for (let star = 9; star >= 1; star -= 2) {
+//   console.log(" ".repeat((9 - star) / 2) + "*".repeat(star));
+// }
+
+const me = {
+  name: "은진",
+  age: 27,
+};
+console.log(me);
