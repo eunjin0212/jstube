79dc6c21c32e6e6a924a186d8eace4375e8564e4
eunjin0212 <eunjin9639@gmail.com>
2020-09-14 15:58:57 +0900

    js

--- test.js
+++ test.js
@@ -7,3 +7,21 @@ console.log(빈값);
 빈값 = null;
 
 console.log(빈값);
+
+console.log(null == undefined);
+console.log(null === undefined);
+var 조건 = 5 * 5 === 25;
+
+if (조건) {
+  return "딩동댕";
+} else {
+  return "땡";
+}
+console.log(조건);
+
+if (false) {
+  ("딩동");
+} else {
+  ("땡");
+}
+console.log(false);
