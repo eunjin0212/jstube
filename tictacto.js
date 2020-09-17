e1844bd997d4e87179e3c615aa6241fd52717007
eunjin0212 <eunjin9639@gmail.com>
2020-09-18 00:25:36 +0900

    js

--- tictacto.js
+++ tictacto.js
@@ -1,85 +1,123 @@
 var body = document.body;
 var table = document.createElement(`table`);
-
 var lines = [];
 var cans = [];
 var turn = "X";
-var result = document.createElement(`div`);
+var 결과 = document.createElement(`div`);
+
+function resultCheck(몇줄, 몇칸) {
+  //세칸 다 채워졌나?
+  var full = false;
+  //가로줄 검사
+  if (
+    cans[몇줄][0].textContent === turn &&
+    cans[몇줄][1].textContent === turn &&
+    cans[몇줄][2].textContent === turn
+  ) {
+    full = true;
+  }
+  //세로줄 검사
+  if (
+    cans[0][몇칸].textContent === turn &&
+    cans[1][몇칸].textContent === turn &&
+    cans[2][몇칸].textContent === turn
+  ) {
+    full = true;
+  }
+  //대각선 검사
+  if (
+    cans[0][0].textContent === turn &&
+    cans[1][1].textContent === turn &&
+    cans[2][2].textContent === turn
+  ) {
+    full = true;
+  }
+  if (
+    cans[2][0].textContent === turn &&
+    cans[1][1].textContent === turn &&
+    cans[0][2].textContent === turn
+  ) {
+    full = true;
+  }
+  return full;
+}
+function reset(무승부) {
+  //초기화
+  if (무승부) {
+    결과.textContent = "무승부";
+  } else {
+    //승부
+    결과.textContent = turn + "님이 승리!";
+  }
+  setTimeout(function () {
+    결과.textContent = "";
+    cans.forEach(function (line) {
+      line.forEach(function (can) {
+        can.textContent = "";
+      });
+    });
+    turn = "X";
+  }, 1000);
+}
 
 var 비동기콜백 = function (event) {
-  console.log(event.target); //칸
-  console.log(event.target.parentNode); //줄
-  console.log(event.target.parentNode.parentNode); //테이블
+  //칸을 클릭했을때
+  if (turn === "O") {
+    //컴퓨터의 턴일 때 내가 클릿하지 않도록
+    return;
+  }
   var 몇줄 = lines.indexOf(event.target.parentNode);
-  console.log("몇줄", 몇줄);
   var 몇칸 = cans[몇줄].indexOf(event.target);
-  console.log("몇칸", 몇칸);
 
-  //칸이 이미 채워져 있는가?
   if (cans[몇줄][몇칸].textContent !== "") {
+    //칸이 이미 채워져 있는가?
+    console.log("빈 칸이 아닙니다");
   } else {
     //빈칸이면
-
+    console.log("빈 칸입니다");
     cans[몇줄][몇칸].textContent = turn;
-    //세칸 다 채워졌나?
-    var full = false;
-    //가로줄 검사
-    if (
-      cans[몇줄][0].textContent === turn &&
-      cans[몇줄][1].textContent === turn &&
-      cans[몇줄][2].textContent === turn
-    ) {
-      full = true;
-    }
-    //세로줄 검사
-    if (
-      cans[0][몇칸].textContent === turn &&
-      cans[1][몇칸].textContent === turn &&
-      cans[2][몇칸].textContent === turn
-    ) {
-      full = true;
-    }
-    //대각선 검사
-    if (몇줄 - 몇칸 === 0) {
-      //대각선 검사 필요한 경우
-      if (
-        cans[0][0].textContent === turn &&
-        cans[1][1].textContent === turn &&
-        cans[2][2].textContent === turn
-      ) {
-        full = true;
-      }
-    }
-    if (Math.abs(몇줄 - 몇칸) === 2) {
-      //대각선 검사 필요한 경우
-      if (
-        cans[2][0].textContent === turn &&
-        cans[1][1].textContent === turn &&
-        cans[0][2].textContent === turn
-      ) {
-        full = true;
-      }
-    }
-    //다 찼으면
-    if (full) {
-      result.textContent = turn + "님이 승리!";
-      //초기화
-      turn = "X";
-      cans.forEach(function (line) {
-        line.forEach(function (can) {
-          can.textContent = "";
-        });
+    var victoryCheck = resultCheck(몇줄, 몇칸);
+    //모든칸이 다 찼는지 검사
+    var 후보칸 = [];
+    cans.forEach(function (line) {
+      line.forEach(function (can) {
+        후보칸.push(can);
       });
+    });
+    후보칸 = 후보칸.filter(function (can) {
+      return !can.textContent;
+    });
+    if (victoryCheck) {
+      //이겼을경우
+      reset();
+    } else if (후보칸.length === 0) {
+      //칸을 더이상 선택할 수 없음
+      reset(true);
     } else {
-      //다안찼으면
       if (turn === "X") {
         turn = "O";
-      } else {
-        turn = "X";
       }
+      setTimeout(function () {
+        console.log("컴퓨터의 턴 입니다.");
+        //빈칸중 하나를 고른다
+        var selectCan = 후보칸[Math.floor(Math.random() * 후보칸.length)];
+        selectCan.textContent = turn;
+        //컴퓨터가 승리했는지 체크
+        var 몇줄 = lines.indexOf(selectCan.parentNode);
+        var 몇칸 = cans[몇줄].indexOf(selectCan);
+        var victoryCheck = resultCheck(몇줄, 몇칸);
+        //다찼으면
+        if (victoryCheck) {
+          //컴퓨터가 이겼을 경우
+          reset();
+        }
+        //턴을 나한테 넘긴다
+        turn = "X";
+      }, 1000);
     }
   }
 };
+
 for (var i = 1; i <= 3; i += 1) {
   var line = document.createElement(`tr`);
   lines.push(line);
@@ -93,5 +131,5 @@ for (var i = 1; i <= 3; i += 1) {
   table.appendChild(line);
 }
 body.appendChild(table);
-body.appendChild(result);
+body.appendChild(결과);
 console.log(lines, cans);
