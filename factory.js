b34fff3d547d4bbddaa968d839197f087c56b15e
eunjin0212 <eunjin9639@gmail.com>
2020-09-18 16:47:06 +0900

    js

--- factory.js
+++ factory.js
@@ -1,12 +1,16 @@
-function cardFactory(name, att, hp) {
-  return {
-    name: name,
-    att: att,
-    hp: hp,
+var 프로토타입 = {
     type: "card",
     attack: function () {},
     defend: function () {},
-  };
+};
+//Object.create(프로토타입)
+function cardFactory(name, att, hp) {
+  var card = Object.create(프로토타입);
+    card.name= name,
+    card.att= att,
+    card.hp= hp,
+  //card.__proto__ = 프로토타입
+  return card;
 }
 
 const card = cardFactory("eunjin", 10, 10);
@@ -28,6 +32,7 @@ const card3 = {
 };
 card3.prototype = protoType;
 // console.log(card3);
-//__proto__는 생략 가능
-//prototype을 쓰는 이유 css랑 비슷함 한방에 바꿀 수 있기 때문에
-console.log(card3.prototype);
+__proto__는 생략 가능 --> 실무에서는 쓰지 말 것!!!!
+대신에 Object.create 쓰기 
+prototype을 쓰는 이유 css랑 비슷함 한방에 바꿀 수 있기 때문에
+// console.log(card3.prototype);
